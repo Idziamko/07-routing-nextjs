bc5562211c49009d94bf2b421f68e23656f632df
Ivan <dziamko02@gmail.com>
2026-04-25 12:19:31 +0300

    fix: add redirect for deleted /notes page

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,15 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   allowedDevOrigins: ["192.168.0.4"],
+  async redirects() {
+    return [
+      {
+        source: '/notes',
+        destination: '/notes/filter/all',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
